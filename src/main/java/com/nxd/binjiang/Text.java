055fa8d306324aefa3968734809f4c09b281e857
OldMountain <1351395043@qq.com>
2017-11-29 16:18:07 +0800

    新建项目，SSM+druid+bootstrap+maven

---
+++ src/main/java/com/nxd/binjiang/Text.java
@@ -0,0 +1,14 @@
+package com.nxd.binjiang;
+
+/**
+ * OrderController
+ *
+ * @author luhangqi
+ * @date 2017/11/29
+ */
+public class Text {
+
+    public static void main(String[] args) {
+        System.out.println("什么也没有");
+    }
+}
