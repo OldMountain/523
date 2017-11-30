a05b532ea39e99218c1fe69f8589cc1d7e08eb78
OldMountain <1351395043@qq.com>
2017-11-30 18:26:20 +0800

    新增一波

--- src/main/java/com/nxd/binjiang/util/SmsUtils.java
+++ src/main/java/com/nxd/binjiang/util/SmsUtils.java
@@ -1,12 +1,5 @@
 package com.nxd.binjiang.util;
 
-import com.alibaba.fastjson.JSONObject;
-import com.nxd.entity.ResultModel;
-import com.nxd.enumtype.ResultEnum;
-
-import java.util.HashMap;
-import java.util.Map;
-
 public class SmsUtils {
     //赛邮
     private static String appid = "16391";
@@ -17,30 +10,30 @@ public class SmsUtils {
     private static String sign = "【nxd】";
 
     public static void main(String[] args) {
-        SmsUtils.sendSMS("Your verification code is: 123456,keep it to yourself!","18251912734");
+//        SmsUtils.sendSMS("Your verification code is: 123456,keep it to yourself!","18251912734");
 //        SmsUtils.sendSMS("您的验证码是：123456。请不要把验证码泄露给其他人。","18251912734");
     }
 
-    public static ResultModel sendSMS(String content, String mobile){
-        ResultModel result = new ResultModel();
-        if(content == null){
-            return result;
-        }
-        Map<String, String> params = new HashMap<>();
-        try {
-            params.put("appid", Const.APPID);
-            params.put("content", sign + content);
-            params.put("to", mobile);
-            params.put("signature", Const.SIGNATURE);
-//            params.put("ExtNo","");//用户定义扩展码，只能为正整数
-            String response = InterfaceUtil.httpPostRequest(postUrlSaiYou, params);
-            JSONObject jsonObject = JSONObject.parseObject(response);
-            if (jsonObject.getString("status").equals("success")) {
-                result.setResult(ResultEnum.SUCCESS);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return result;
-    }
+//    public static ResultModel sendSMS(String content, String mobile){
+//        ResultModel result = new ResultModel();
+//        if(content == null){
+//            return result;
+//        }
+//        Map<String, String> params = new HashMap<>();
+//        try {
+//            params.put("appid", Const.APPID);
+//            params.put("content", sign + content);
+//            params.put("to", mobile);
+//            params.put("signature", Const.SIGNATURE);
+////            params.put("ExtNo","");//用户定义扩展码，只能为正整数
+//            String response = InterfaceUtil.httpPostRequest(postUrlSaiYou, params);
+//            JSONObject jsonObject = JSONObject.parseObject(response);
+//            if (jsonObject.getString("status").equals("success")) {
+//                result.setResult(ResultEnum.SUCCESS);
+//            }
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//        return result;
+//    }
 }
